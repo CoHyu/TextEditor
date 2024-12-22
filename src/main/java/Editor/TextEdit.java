94283717ae112147a646b32774b0aca136c1c667
CoHyu <3583123755@qq.com>
2024-12-22 20:31:35 +0800

    2nd Change.

---
+++ src/main/java/Editor/TextEdit.java
@@ -0,0 +1,102 @@
+package Editor;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class TextEdit extends JFrame implements ActionListener {
+    private static JTextArea area;
+    private static JFrame frame;
+
+    public TextEdit() {
+        run();
+    }
+
+    public void run() {
+        frame = new JFrame("Text Edit");
+
+        // 设置外观和感觉
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(TextEdit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // 设置窗口属性
+        area = new JTextArea();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(area);
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+
+        // 创建菜单栏
+        JMenuBar menuMain = new JMenuBar();
+        JMenu menuFile = new JMenu("File");
+
+        JMenuItem menuItemNew = new JMenuItem("New");
+        JMenuItem menuItemOpen = new JMenuItem("Open");
+        JMenuItem menuItemSave = new JMenuItem("Save");
+        JMenuItem menuItemQuit = new JMenuItem("Quit");
+
+        menuItemNew.addActionListener(this);
+        menuItemOpen.addActionListener(this);
+        menuItemSave.addActionListener(this);
+        menuItemQuit.addActionListener(this);
+
+        menuMain.add(menuFile);
+        menuFile.add(menuItemNew);
+        menuFile.add(menuItemOpen);
+        menuFile.add(menuItemSave);
+        menuFile.add(menuItemQuit);
+
+        frame.setJMenuBar(menuMain);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String ingest = null;
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Choose destination.");
+        jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+        String ae = e.getActionCommand();
+        if (ae.equals("Open")) {
+            int returnValue = jfc.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File f = new File(jfc.getSelectedFile().getAbsolutePath());
+                try {
+                    FileReader read = new FileReader(f);
+                    Scanner scan = new Scanner(read);
+                    while (scan.hasNextLine()) {
+                        String line = scan.nextLine() + "\n";
+                        ingest = (ingest == null ? "" : ingest) + line;
+                    }
+                    area.setText(ingest);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } else if (ae.equals("Save")) {
+            int returnValue = jfc.showSaveDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                try {
+                    File f = new File(jfc.getSelectedFile().getAbsolutePath());
+                    FileWriter out = new FileWriter(f);
+                    out.write(area.getText());
+                    out.close();
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(null, "Error while saving file.");
+                }
+            }
+        } else if (ae.equals("New")) {
+            area.setText("");
+        } else if (ae.equals("Quit")) {
+            System.exit(0);
+        }
+    }
+}
--- src/main/java/first/mak.java
+++
@@ -1,7 +0,0 @@
-package first;
-
-public class mak {
-    public static void main(String[] args) {
-        System.out.println("11");
-    }
-}
